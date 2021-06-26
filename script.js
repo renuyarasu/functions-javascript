c9b20e623eaf061847c5328abac95bd9dd97f6bb
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-26 19:38:58 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-// Javascript: Functions
+// Javascript: Active Nav Link Indicator
 
-// this keyword
-// To be little clearer: the 'this' keyword reference the object its was called against
-
-// Callback Functions
-
-function name(x, callBack) {
-    console.log('Renu');
-    console.log(x);
-    callBack();
-}
-function company() {
-    console.log('innRoad');
-}
-
-name('Hello', company);
