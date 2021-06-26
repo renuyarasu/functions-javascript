542162cb87fecc7072989109b9a5cb6acbb71f1b
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-26 18:15:26 +0530

    Arrow Functions

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // Javascript: 
 
+/* 
 // Functions Declaration
-
-/* function greet(name) {
+function greet(name) {
     return `Hi, ${name}!`;
 }
-console.log(greet('Renu'));
- */
+console.log(greet('Renu')); */
 
-// Functions Expression
 
+/*// Functions Expression
 const squareME = function (x) {
     return x * x;
 }
-console.log(squareME(2)); // 4
+console.log(squareME(2)); // 4 */
+
+// Arrow Functions
+/* const sum = function (x, y) {
+    const total = x + y;
+    return total;
+}
+console.log(sum(5, 15)); //20  */
+
+const sum = (x, y) => x + y;
+console.log(sum(5, 15)); //20 
+
