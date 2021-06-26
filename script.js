18ce8a8d1d686f19ecc8c655aaba8ae177a725bb
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-26 18:51:35 +0530

    Immediately Invoked Function Expression - IIFE

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
-// Javascript:
+// Javascript: Functions
 
-/*
-// Functions Declaration
-function greet(name) {
-    return `Hi, ${name}!`;
-}
-console.log(greet('Renu')); */
+// this keyword
+// To be little clearer: the 'this' keyword reference the object its was called against
 
+// Immediately Invoked Function Expression - IIFE
 
-/*// Functions Expression
-const squareME = function (x) {
-    return x * x;
-}
-console.log(squareME(2)); // 4 */
+/* (function () {
+    console.log('You have Invoked me!');
+})() */
 
-// Arrow Functions
-/* const sum = function (x, y) {
-    const total = x + y;
-    return total;
-}
-
-const sum = (x, y) => x + y;
-console.log(sum(5, 15)); //20
-console.log(sum(5, 15)); //20  */
-
-/* const newStudent = function (fName, lName, age) {
-    const student = {
-        name: `${fName} ${lName}`,
-        age,
-    }
-    return student;
-}
-
-console.log(newStudent('VedaGna', 'Yarasu', 5)); */
-
-const newStudent = (fName, lName, age) => ({name: `${fName} ${lName}`, age})
-
-
-console.log(newStudent('VedaGna', 'Yarasu', 5));
+(function (name) {
+    console.log(`${name}, You have Invoked me!` );
+})('Renu')
