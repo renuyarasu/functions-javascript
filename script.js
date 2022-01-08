fb0f9b0f7cbe1048f7e8a0704b11d7767048b2a5
Renuka Prasad <renu.yarasu@gmail.com>
2022-01-08 21:10:01 +0530

    Javascript: Comparison with the Strict Equality Operator

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// Javascript: Comparison with the Equality Operator
+// Javascript: Comparison with the Strict Equality Operator
 console.clear();
 
-function testEquality(val) {
-    if (val == 10) {
+/* function testEquality(val) {
+    if (val === 10) {
         return 'Equal';
     }
     return 'Not Equal';
 }
-console.log(testEquality(10));
+console.log(testEquality('10')); */
+
+function comparisonEquality(a, b) {
+    if (a === b) {
+        return 'Equal';
+    }
+    return 'Not Equal';
+}
+
+console.log(comparisonEquality(10, "10"));
