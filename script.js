29c399fc19aa9d42623276cd73432b167cf42c21
Renuka Prasad <renu.yarasu@gmail.com>
2022-01-10 19:54:34 +0530

    Javascript: Golf Game Code

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-// Javascript: Chaining If else statements
+// Javascript: Golf Game Code
 console.clear();
 
-function testSize(num) {
-    if (num < 5) {
-        return 'Tiny'
-    }
-    if (num < 10) {
-        return 'Small'
-    }
-    if (num < 15) {
-        return 'Medium'
-    }
-    if (num < 20) {
-        return 'Large'
-    }
-    if (num >= 20) {
-        return 'Huge'
+let names = ['Hole-in-one!', 'Eagle', 'Birdie', 'Par', 'Bogey', 'Double Bogey', 'Go Home!']
+
+function golfScore(par, strokes) {
+    if (strokes == 2) {
+        return names[0]
+    } else if (strokes <= par - 2) {
+        return names[1]
+    } else if (strokes == par - 1) {
+        return names[2]
+    } else if (strokes == par) {
+        return names[3]
+    } else if (strokes == par + 1) {
+        return names[4]
+    } else if (strokes == par + 2) {
+        return names[5]
+    } else if (strokes >= par + 3) {
+        return names[6]
     }
 }
-console.log(testSize(50));
+console.log(golfScore(5, 10));
