6657c508e0bd7b55d723ee40de8aea8f7518202c
Renuka Prasad <renu.yarasu@gmail.com>
2022-01-28 18:14:26 +0530

    Javascript: Objects - Testing Objects for Properties

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 // Javascript: Updating Object Properties
 console.clear();
 
-function numberLookups(val) {
-    let result = '';
-
-    let lookup = {
-        1: 'One',
-        2: 'Two',
-        3: 'Three',
-        4: 'Four',
-        5: 'Five',
-        6: 'Six',
-        7: 'Seven',
-        8: 'Eight',
-        9: 'Nine',
-        10: 'TEN'
-    };
-
-    result = lookup[val];
-    return result;
+let myObj = {
+    name: 'VedaGna',
+    age: 5,
+    sib: 'Sister'
 }
 
-console.log(numberLookups(7));
+function checkObj(checkProp) {
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp]
+    } else {
+        return 'Not Found!'
+    }
+}
+
+console.log(checkObj('sib'));
