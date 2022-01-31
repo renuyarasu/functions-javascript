3e1b17cd56714c722fe872b940fe95d49fec9b88
Renuka Prasad <renu.yarasu@gmail.com>
2022-01-31 22:00:43 +0530

    Javascript: Iterate with a While Loop

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 // Javascript: Updating Object Properties
 console.clear();
 
-let myGarden = [
-    {
-        type: 'Flower',
-        list: ['Rose', 'Lily', 'Tulip', 'Orchid', 'Carnation', 'Hyacinth', 'PeruvianLily', 'Chrysanthemum']
-    },
-    {
-        type: 'Trees',
-        list: ['Vepa', "Fir", 'Pine', 'Fabaceae', 'Labernum', 'CoralTree']
-    }
-]
+let myArray = [];
+let i = 0;
 
-let findCoralTree = myGarden[1].list[5]
-console.log(findCoralTree);
+while (i < 10) {
+    myArray.push(i);
+    i++;
+}
+console.log(myArray);
