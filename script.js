092afedfa0c46ca9b3a6bd631243782ea5a87497
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-01 18:48:50 +0530

    Javascript: Do...While Loops

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-// Javascript: Nesting For Loops
+// Javascript: Do...While Loops
 console.clear();
 
-function multiplyAll(arr) {
-    let product = 1;
+let arr = [];
+let i = 10;
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            product *= arr[i][j];
-
-        }
-    }
-    return product;
-}
-
-let product = multiplyAll([[1, 2], [3, 4,], [5, 6, 7]]);
-
-console.log(product);
+do {
+    arr.push(i);
+    i++
+} while (i < 5)
+console.log(i, arr);
