06438a34d5557314164bf560729ea6c2e6660483
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-01 19:11:10 +0530

    Javascript: Profile Lookup

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-// Javascript: Do...While Loops
+// Javascript: Profile Lookup
 console.clear();
 
-let arr = [];
-let i = 10;
+let contacts = [
+    {
+        fName: 'VedaGna',
+        lName: 'Yarasu',
+        number: 001,
+        likes: ['grapes', 'lime', 'lemon', 'cherry', 'blueberry', 'banana', 'apple', 'watermelon']
+    },
+    {
+        fName: 'Gnapika',
+        lName: 'Yarasu',
+        number: 002,
+        likes: ['Cheese', 'Egg', 'Butter', 'Margarine', 'Yogurt', 'Cottage cheese', 'Ice cream']
+    },
+    {
+        fName: 'Teja',
+        lName: 'Yarasu',
+        number: 003,
+        likes: ['Almonds', 'Mixed Nuts', 'Mixed Dried Fruits', 'Cashews', 'Walnuts', 'Raisins', 'Pistachios']
+    },
+    {
+        fName: 'Hindu',
+        lName: 'Yarasu',
+        number: 004,
+        likes: ['Pearl millet', 'Finger millet', 'Foxtail millet', 'Proso millet']
+    },
+];
 
-do {
-    arr.push(i);
-    i++
-} while (i < 5)
-console.log(i, arr);
+function lookupProfile(name, prop) {
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].fName === name) {
+            return contacts[i][prop] || 'No Such Property!'
+        }
+    }
+    return 'No Such Contact!';
+}
+
+let data = lookupProfile('Teja', 'likes');
+
+console.log(data);
