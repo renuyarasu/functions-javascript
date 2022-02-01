703bbb4817c99c3d02f5c9701162f26be5c24b84
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-01 19:15:57 +0530

    Javascript: Generate Random Fractions

--- script.js
+++ script.js
@@ -1,42 +1,7 @@
-// Javascript: Profile Lookup
+// Javascript: Generate Random Fractions
 console.clear();
 
-let contacts = [
-    {
-        fName: 'VedaGna',
-        lName: 'Yarasu',
-        number: 001,
-        likes: ['grapes', 'lime', 'lemon', 'cherry', 'blueberry', 'banana', 'apple', 'watermelon']
-    },
-    {
-        fName: 'Gnapika',
-        lName: 'Yarasu',
-        number: 002,
-        likes: ['Cheese', 'Egg', 'Butter', 'Margarine', 'Yogurt', 'Cottage cheese', 'Ice cream']
-    },
-    {
-        fName: 'Teja',
-        lName: 'Yarasu',
-        number: 003,
-        likes: ['Almonds', 'Mixed Nuts', 'Mixed Dried Fruits', 'Cashews', 'Walnuts', 'Raisins', 'Pistachios']
-    },
-    {
-        fName: 'Hindu',
-        lName: 'Yarasu',
-        number: 004,
-        likes: ['Pearl millet', 'Finger millet', 'Foxtail millet', 'Proso millet']
-    },
-];
-
-function lookupProfile(name, prop) {
-    for (let i = 0; i < contacts.length; i++) {
-        if (contacts[i].fName === name) {
-            return contacts[i][prop] || 'No Such Property!'
-        }
-    }
-    return 'No Such Contact!';
+function randomFractions() {
+    return Math.random();
 }
-
-let data = lookupProfile('Teja', 'likes');
-
-console.log(data);
+console.log(randomFractions());
