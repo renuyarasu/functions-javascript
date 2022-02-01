9f1f5159983b7c1ef50971b78dc08ca23d348168
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-01 21:07:41 +0530

    Javascript: var vs let scopes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-// Javascript: Difference Between var and let
+// Javascript: var vs let scopes
 console.clear();
-
-let animal = 'Cat';
-animal = 'Dog';
-
-console.log(animal);
+"use strict"
+function checkScope() {
+    let i = 'Function Scope';
+    if (true) {
+        let i = 'Block Scope';
+        console.log('Block Scope i is:', i);
+    }
+    console.log('Function Scope i is:', i);
+    return i;
+}
+let result = checkScope();
