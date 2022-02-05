4eda42d768bc98dc8b2280183999bcc20c44efea
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-05 22:33:00 +0530

    Javascript: Destructuring Assignment - Pass an Object as a Function Parameters

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// Javascript: Destructuring Assignment - Rest Operator to Reassign Array
+// Javascript: Destructuring Assignment - Pass an Object as a Function's Parameters
 console.clear();
 
-const numbers = [1, 2, 3, 4, 5];
-function removeFirstTwo(list) {
-    const [, , ...arr] = list;
-    return arr;
-}
-const arr = removeFirstTwo(numbers);
-console.log(arr); // [3, 4, 5]
-console.log(numbers); // [1, 2, 3, 4, 5]
+const stats = {
+    max: 55.55,
+    min: 5.5,
+    standard: 2.2,
+    mode: 23.3,
+    avg: 6.5,
+};
+
+/* const half = (function () {
+    return function half(stats) {
+        return (stats.max + stats.min) / 2.0;
+    }
+})(); */
+
+const half = (function () {
+    return function half({ max, min }) {
+        return (max + min) / 2.0
+    }
+})();
+
+console.log(stats); //{ max: 55.55, min: 5.5, standard: 2.2, mode: 23.3, avg: 6.5 }
+console.log(half(stats)); // 30.525
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-max
+stats
