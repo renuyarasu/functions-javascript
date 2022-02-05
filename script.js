cd9fec0c687a3e092583cdee1e05fc7c775898da
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-05 22:51:16 +0530

    Javascript: Create Strings using Template Literals - <li>

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // Javascript: Create Strings using Template Literals
 console.clear();
 
-const person = {
-    name: 'VedaGna',
-    age: 5
-}
-const greets = `Hello ${person.name}! I'm ${person.age} years old!`;
-console.log(greets); // Hello VedaGna! I'm 5 years old!
+const result = {
+    success: ['success_01', 'success_02', 'success_03'],
+    failure: ['failure_01', 'failure_02', 'failure_03'],
+    skipped: ['skipped_01', 'skipped_02', 'skipped_03'],
+};
 
+
+function makeList(arr) {
+    const resultDisplayArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        resultDisplayArray.push(`<li>${arr[i]}</li>`)
+    }
+
+    return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList(result.success);
+
+console.log(resultDisplayArray);
