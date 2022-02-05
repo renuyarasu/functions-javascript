50ff672250e06d8cd66bcb2b7b8ffabe81117df1
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-05 22:16:49 +0530

    Javascript: Destructuring Assignment - Arrays

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Javascript: Destructuring Assignment - Nested Objects
+// Javascript: Destructuring Assignment - Arrays
 console.clear();
 
-const LOCAL_FORECAST = {
-    today: { min: 50, max: 100 },
-    tomorrow: { min: 100, max: 200 },
-};
+const [x, y, , z] = [1, 2, 3, 4, 5];
+console.log(x, y, z); // 1 2 4
 
-function getMaxOfTomorrow(forecast) {
+let a = 10, b = 20;
+(() => {
     'use strict';
-    const { tomorrow: { max: maxOfTomorrow } } = forecast;
-    return maxOfTomorrow;
-}
-console.log(getMaxOfTomorrow(LOCAL_FORECAST)); // 200
+    [a, b] = [b, a];
+})();
+console.log(a); // 20
+console.log(b); // 10
