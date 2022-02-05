c7195c9c9d21935f0cf3cab1d755cbc4b448e5c1
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-05 22:43:32 +0530

    Javascript: Create Strings using Template Literals

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-// Javascript: Destructuring Assignment - Pass an Object as a Function's Parameters
+// Javascript: Create Strings using Template Literals
 console.clear();
 
-const stats = {
-    max: 55.55,
-    min: 5.5,
-    standard: 2.2,
-    mode: 23.3,
-    avg: 6.5,
-};
+const person = {
+    name: 'VedaGna',
+    age: 5
+}
+const greets = `Hello ${person.name}! I'm ${person.age} years old!`;
+console.log(greets); // Hello VedaGna! I'm 5 years old!
 
-/* const half = (function () {
-    return function half(stats) {
-        return (stats.max + stats.min) / 2.0;
-    }
-})(); */
-
-const half = (function () {
-    return function half({ max, min }) {
-        return (max + min) / 2.0
-    }
-})();
-
-console.log(stats); //{ max: 55.55, min: 5.5, standard: 2.2, mode: 23.3, avg: 6.5 }
-console.log(half(stats)); // 30.525
